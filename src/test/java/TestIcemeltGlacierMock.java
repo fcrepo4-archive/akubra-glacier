b6ef186149915d7f575e145811444fd681360e38
Chris Beer <cabeer@stanford.edu>
2012-12-16 11:51:52 -0800

    initial commit, showing icemelt works.

---
+++ src/test/java/TestIcemeltGlacierMock.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.io.IOException;
+import com.amazonaws.services.glacier.*;
+import com.amazonaws.services.glacier.model.*;
+import com.amazonaws.auth.PropertiesCredentials;
+
+public class TestIcemeltGlacierMock {
+
+	@Test
+	public void testCreateVault() throws IOException {
+		PropertiesCredentials credentials = new PropertiesCredentials(TestIcemeltGlacierMock.class.getResourceAsStream("AwsCredentials.properties"));
+		AmazonGlacierClient client = new AmazonGlacierClient(credentials);
+		client.setEndpoint("http://localhost:3000/");
+		
+		CreateVaultRequest request = new CreateVaultRequest()
+	    .withAccountId("-")
+	    .withVaultName("akubra-glacier-vault");
+	    CreateVaultResult result = client.createVault(request);
+	}
+
+}
